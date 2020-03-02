bc89e6a89962a7a535416febabe7c75aec8465f0
indiegameband <indiegameband@gmail.com>
2020-03-03 00:49:22 +0300

    HW_16_Final

--- HW_16.cpp
+++ HW_16.cpp
@@ -3,24 +3,37 @@
 
 #include <iostream>
 
-
-
 int main()
 {
-    const int Nsize = 5;
-    int array[Nsize][Nsize];
+    const int Nsize = 5; // Size of array
+    int array[Nsize][Nsize]; 
 
 
     for (int i = 0; i < Nsize; i++)
     {
+        std::cout << "String "<< i<<" : ";
+
         for (int j = 0; j < Nsize; j++)
         {
             array[i][j] = i + j;
-            std::cout << array[i][j];
+            std::cout <<  array[i][j] << " "; //Filling array
         }
         std::cout << "\n";
     }
 
+    int Date;
 
+    std::cout << "\n" << "Enter current date: ";
+    std::cin >> Date;
+    int StringNumber = Date % Nsize; //Division of date and array size
+
+    int Sum = 0; //Entering integer for counting sum in array
+    
+    for (int String = 0; String < Nsize; String++)
+    {
+        Sum = Sum + array[StringNumber][String];
+    }
+
+    std::cout <<"\n" << "Sum of string " <<StringNumber <<" = " <<Sum<< "\n"; //Output result of string
 }
 
